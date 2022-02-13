e5169f4f543b8ad4247ecff5ee3ed2e4dac30123
Caroline P. da Silva <carolinepedasil@gmail.com>
2022-02-13 18:40:15 -0300

    Java

---
+++ Alura/Certificação Java/Certificação Java SE 7 Programmer I: Tipos de Dados/Teste.java
@@ -0,0 +1,26 @@
+public class Teste {
+
+    public static void main(String[] args) {
+
+        char c = 'A';
+        System.out.println(c);
+
+        boolean valor = true;
+
+        long x2 = 1_234_567_891_234l;
+
+        int x = 15;
+        double v = 10.2d;
+        long l = 13l;
+        float f = 13.1f;
+
+        double d2 = 3.1E2;
+        System.out.println(d2);
+
+        float f2 = 2e3f;
+
+        int i = 0B100;
+        System.out.println(i);
+
+    }
+}
