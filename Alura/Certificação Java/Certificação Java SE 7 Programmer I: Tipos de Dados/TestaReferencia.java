60c7d02fecbfc587adb26102b0826cad66d0c0b7
Caroline P. da Silva <carolinepedasil@gmail.com>
2022-02-14 22:39:35 -0300

    Java

---
+++ Alura/Certificação Java/Certificação Java SE 7 Programmer I: Tipos de Dados/TestaReferencia.java
@@ -0,0 +1,22 @@
+class ObjetoMeu {
+    int valor;
+}
+
+public class TesteReferencia {
+    public static void main(String[] args) {
+        ObjetoMeu o1 = new ObjetoMeu();
+        o1.valor = 10;
+        ObjetoMeu o2 = o1;
+
+        System.out.println(o1.valor);
+        System.out.println(o2.valor);
+
+        int a = 10;
+        int b = a;
+
+        a = a + 5;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
