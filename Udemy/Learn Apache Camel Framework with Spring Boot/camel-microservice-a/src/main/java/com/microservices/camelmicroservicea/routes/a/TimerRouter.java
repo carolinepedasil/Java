dae03c05a9cfab24bd4ddc2165662d8c62260e6a
Caroline Pereira da Silva <carolinepedasil@gmail.com>
2022-03-21 19:35:10 -0300

    Java

--- Udemy/Learn Apache Camel Framework with Spring Boot/camel-microservice-a/src/main/java/com/microservices/camelmicroservicea/routes/a/TimerRouter.java
+++ Udemy/Learn Apache Camel Framework with Spring Boot/camel-microservice-a/src/main/java/com/microservices/camelmicroservicea/routes/a/TimerRouter.java
@@ -3,11 +3,20 @@ package com.microservices.camelmicroservicea.routes.a;
 import java.time.LocalDateTime;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component //importar do stereotype.Component
 public class TimerRouter extends RouteBuilder {
 
+	@Autowired
+	private GetCurrentTimeBean getCurrentTimeBean;
+	
+	@Autowired
+	private SimpleLoggingProcessingComponent loggingComponent;
+	
 	@Override
 	public void configure() throws Exception {
 		//timer
@@ -15,9 +24,39 @@ public class TimerRouter extends RouteBuilder {
 		//log
 		//Exchange[ExchangePattern: InOnly, BodyType: null, Body: [Body is null]]
 		from("timer:first-timer") //timer endpoint - como se fosse o queue
-//		.transform().constant("My Constant Message") //transformar a mensagem em constante
-		.transform().constant("Time now is" + LocalDateTime.now())
+		.log("${body}")
+		.transform().constant("My Constant Message") //transformar a mensagem em constante
+		.log("${body}")
+//		.transform().constant("Time now is" + LocalDateTime.now())
+//		.bean(getCurrentTimeBean, "getCurrentTime1")
+		
+		//Processing
+		//Transformation
+		
+		.bean(getCurrentTimeBean)
+		.log("${body}")
+		.bean(loggingComponent)
+		.log("${body}")
 		.to("log:first-timer"); //mandar para o log - como se fosse o banco de dados/database
 	}
 
+}
+
+@Component
+class GetCurrentTimeBean {
+	public String getCurrentTime() {
+		return "Time now is" + LocalDateTime.now();
+	}
+}
+
+@Component
+class SimpleLoggingProcessingComponent {
+	
+	private Logger logger = LoggerFactory.getLogger(SimpleLoggingProcessingComponent.class);
+	
+	public void process(String message) {
+		
+		logger.info("SimpleLoggingProcessingComponent {}", message);
+		
+	}
 }
