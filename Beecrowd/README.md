7b9bb0e97fa2c45f2cfac49dc08adf56a44bf114
Caroline P. da Silva <carolinepedasil@gmail.com>
2022-03-24 08:50:39 -0300

    Java

--- Beecrowd/README.md
+++ Beecrowd/README.md
@@ -10,7 +10,7 @@
 | [1005](https://www.beecrowd.com.br/judge/pt/problems/view/1005) | [Média 1](https://github.com/carolinepedasil/Java/blob/master/Beecrowd/URI1005.java)             | ✅        |
 | [1006](https://www.beecrowd.com.br/judge/pt/problems/view/1006) | [Média 2](https://github.com/carolinepedasil/Java/blob/master/Beecrowd/URI1006.java)             | ✅        |
 | [1007](https://www.beecrowd.com.br/judge/pt/problems/view/1007) | [Diferença](https://github.com/carolinepedasil/Java/blob/master/Beecrowd/URI1007.java)           | ✅        |
-| [1008](https://www.beecrowd.com.br/judge/pt/problems/view/1008) | Salário                                  |           |
+| [1008](https://www.beecrowd.com.br/judge/pt/problems/view/1008) | [Salário](https://github.com/carolinepedasil/Java/blob/master/Beecrowd/URI1008.java)             | ✅        |
 | [1009](https://www.beecrowd.com.br/judge/pt/problems/view/1009) | Salário com Bônus                        |           |
 | [1010](https://www.beecrowd.com.br/judge/pt/problems/view/1010) | Cálculo Simples                          |           |
 | [1011](https://www.beecrowd.com.br/judge/pt/problems/view/1011) | Esfera                                   |           |
---
+++ Beecrowd/URI1008.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
+/*
+    Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse funcionário. 
+    A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
+
+    ENTRADA:
+    O arquivo de entrada contém 2 números inteiros e 1 número com duas casas decimais, representando o número, quantidade de horas trabalhadas e o valor que o funcionário recebe por hora trabalhada, respectivamente.
+
+    SAÍDA:
+    Imprima o número e o salário do funcionário, conforme exemplo fornecido, com um espaço em branco antes e depois da igualdade. 
+    No caso do salário, também deve haver um espaço em branco após o $.
+*/
+
+public class URI1008 {
+ 
+    public static void main(String[] args) throws IOException {
+
+        Locale.setDefault(Locale.US);
+ 
+        Scanner sc = new Scanner(System.in);
+        
+        int funcionarios = sc.nextInt();
+        int horasTrabalhadas = sc.nextInt();
+        double valorHora = sc.nextDouble();
+        
+        double salario = horasTrabalhadas * valorHora;
+        
+        System.out.println("NUMBER = " + funcionarios);
+        System.out.println(String.format("SALARY = U$ %.2f" , salario));
+    }
+ 
+}
