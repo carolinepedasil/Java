47cdfe2e838babc718231346e3e4ab1b64c8ed0d
Caroline P. da Silva <carolinepedasil@gmail.com>
2022-04-06 19:14:06 -0300

    Java

--- Udemy/Learn Apache Camel Framework with Spring Boot/camel-microservice-a/src/main/java/com/microservices/camelmicroservicea/routes/patterns/EipPatternsRouter.java
+++ Udemy/Learn Apache Camel Framework with Spring Boot/camel-microservice-a/src/main/java/com/microservices/camelmicroservicea/routes/patterns/EipPatternsRouter.java
@@ -1,10 +1,16 @@
 package com.microservices.camelmicroservicea.routes.patterns;
 
+import java.util.List;
+
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EipPatternsRouter extends RouteBuilder {
+	
+	@Autowired
+	SplitterComponent splitter;
 	
 	@Override
 	public void configure() throws Exception {
@@ -16,10 +22,24 @@ public class EipPatternsRouter extends RouteBuilder {
 //		.multicast()
 //		.to("log:1", "log:2");
 		
-		from("file:files/csv")
-		.unmarshal().csv()
-		.split(body()) //1 input que é divido em várias mensagens e enviado para a fila
-		.to("activemq:split-queue");
+//		from("file:files/csv")
+//		.unmarshal().csv()
+//		.split(body()) //1 input que é divido em várias mensagens e enviado para a fila
+//		.to("activemq:split-queue");
 		
+		//Message, Message2, Message3
+		from("file:files/csv")
+		.convertBodyTo(String.class)
+		//.split(body(), ",")
+		.split(method(splitter))
+		.to("activmq:split-queue");
+		
+	}
+}
+
+@Component
+class SplitterComponent{
+	public List<String> splitInput(String body){
+		return List.of("ABC", "DEF", "GHI"); //exemplo
 	}
 }
