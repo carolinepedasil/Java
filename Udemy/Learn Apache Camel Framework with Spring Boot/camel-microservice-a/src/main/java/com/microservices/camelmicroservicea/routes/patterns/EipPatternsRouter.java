c28ee3b7c5d93b824629aed315648167b4f710dc
Caroline P. da Silva <carolinepedasil@gmail.com>
2022-04-16 23:22:03 -0300

    Java

--- Udemy/Learn Apache Camel Framework with Spring Boot/camel-microservice-a/src/main/java/com/microservices/camelmicroservicea/routes/patterns/EipPatternsRouter.java
+++ Udemy/Learn Apache Camel Framework with Spring Boot/camel-microservice-a/src/main/java/com/microservices/camelmicroservicea/routes/patterns/EipPatternsRouter.java
@@ -2,13 +2,26 @@ package com.microservices.camelmicroservicea.routes.patterns;
 
 import java.util.List;
 
+import org.apache.camel.AggregationStrategy;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EipPatternsRouter extends RouteBuilder {
 	
+	public class ArrayListAggregationStrategy implements AggregationStrategy {
+
+		@Override
+		public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+	}
+
 	@Autowired
 	SplitterComponent splitter;
 	
@@ -28,11 +41,43 @@ public class EipPatternsRouter extends RouteBuilder {
 //		.to("activemq:split-queue");
 		
 		//Message, Message2, Message3
-		from("file:files/csv")
-		.convertBodyTo(String.class)
-		//.split(body(), ",")
-		.split(method(splitter))
-		.to("activmq:split-queue");
+//		from("file:files/csv")
+//		.convertBodyTo(String.class)
+//		//.split(body(), ",")
+//		.split(method(splitter))
+//		.to("activmq:split-queue");
+		
+		//Aggregate
+		//Messages => Aggregate => Endpoint
+		//to , 3
+		from("file:files/aggregate-json")
+		.unmarshal().json(JsonLibrary.Jackson, CurrencyExchange.class)
+		.aggregate(simple("{body.to}"), new ArrayListAggregationStrategy())
+		.completionSize(3)
+//		.completionTimeout(HIGHEST)
+		.to("log:aggregate-json");
+		
+		String routingSlip = "direct:endpoint1,direct:endpoint2,direct:endpoint3";
+		//String routingSlip = "direct:endpoint1,direct:endpoint2,direct:endpoint3";
+		
+		from("timer:routingSlip?period=10000")
+		.transform().constant("My Message is Hardcoded")
+		.routingSlip(simple(routingSlip));
+		
+		from("direct:endpoint1")
+		.to("log:directendpoint1");
+		
+		from("direct:endpoint2")
+		.to("log:directendpoint2");
+		
+		from("direct:endpoint3")
+		.to("log:directendpoint3");
+		
+		
+		//routing slip
+		//		from("timer:multicast?period=10000")
+		//		.multicast()
+		//		.to("log:something1", "log:something2", "log:something3");
 		
 	}
 }
