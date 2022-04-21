99f239fe9fb0844fb62db13824f59523bff356d5
Caroline P. da Silva <carolinepedasil@gmail.com>
2022-04-20 22:02:34 -0300

    Java

--- Beecrowd/URI1048.java
+++ Beecrowd/URI1048.java
@@ -2,6 +2,25 @@ import java.io.IOException;
 import java.util.Locale;
 import java.util.Scanner;
 import java.text.DecimalFormat;
+
+/*
+	A empresa ABC resolveu conceder um aumento de salários a seus funcionários de acordo com a tabela abaixo:
+
+	Salário					Percentual de Reajuste
+	0 - 400.00				15%
+	400.01 - 800.00			12%
+	800.01 - 1200.00		10%
+	1200.01 - 2000.00		7%
+	Acima de 2000.00		4%
+
+	Leia o salário do funcionário e calcule e mostre o novo salário, bem como o valor de reajuste ganho e o índice reajustado, em percentual.
+
+	ENTRADA:
+	A entrada contém apenas um valor de ponto flutuante, com duas casas decimais.
+
+	SAÍDA:
+	Imprima 3 linhas na saída: o novo salário, o valor ganho de reajuste e o percentual de reajuste ganho, conforme exemplo abaixo.
+*/
  
 public class URI1048 {
  
