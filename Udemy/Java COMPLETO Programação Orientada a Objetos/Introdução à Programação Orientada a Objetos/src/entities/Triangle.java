f3be6e9a089f345bce1ae77f73fb0a51250aa0d0
Caroline P. da Silva <carolinepedasil@gmail.com>
2022-05-20 23:04:05 -0300

    Java

--- Udemy/Java COMPLETO Programação Orientada a Objetos/Introdução à Programação Orientada a Objetos/src/entities/Triangle.java
+++ Udemy/Java COMPLETO Programação Orientada a Objetos/Introdução à Programação Orientada a Objetos/src/entities/Triangle.java
@@ -5,5 +5,21 @@ public class Triangle {
 	public double a;
 	public double b;
 	public double c;
+	
+	/*
+	 	public = indica que essa função poderá ser utilizada em outro arquivo
+	 	double = a função vai retornar um valor tipo double
+	 	depois digitar o nome da operação 
+	 	dentro do () vai vir os parametros que essa função recebe para fazer o que ela tem que fazer
+	*/
+	
+	public double area() {
+		double p = (a + b + c) / 2.0;
+		return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+		/*
+		double result = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+		return result;
+		*/
+	}
 
 }
