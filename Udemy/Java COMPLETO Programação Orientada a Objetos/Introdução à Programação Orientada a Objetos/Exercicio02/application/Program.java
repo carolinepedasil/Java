08608bbad66842c41816f7f4df9f49fd019608b3
Caroline P. da Silva <carolinepedasil@gmail.com>
2022-05-23 22:25:04 -0300

    Java

--- Udemy/Java COMPLETO Programação Orientada a Objetos/Introdução à Programação Orientada a Objetos/Exercicio02/application/Program.java
+++ Udemy/Java COMPLETO Programação Orientada a Objetos/Introdução à Programação Orientada a Objetos/Exercicio02/application/Program.java
@@ -29,7 +29,24 @@ public class Program {
 		System.out.println("Quantity in stock: ");
 		product.quantity = sc.nextInt();
 		
-		System.out.println(product); 
+		System.out.println(); 
+		System.out.println("Product data: " + product); 
+		
+		System.out.println(); 
+		System.out.println("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity); // Atualiza a quantidade dentro do objeto Product
+		
+		System.out.println(); 
+		System.out.println("Updated data: " + product); 
+		
+		System.out.println(); 
+		System.out.println("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		
+		System.out.println(); 
+		System.out.println("Updated data: " + product); 
 		
 		sc.close();
 
--- Udemy/Java COMPLETO Programação Orientada a Objetos/Introdução à Programação Orientada a Objetos/Exercicio02/entities/Product.java
+++ Udemy/Java COMPLETO Programação Orientada a Objetos/Introdução à Programação Orientada a Objetos/Exercicio02/entities/Product.java
@@ -5,25 +5,25 @@ public class Product {
 	public String name;
 	public double price;
 	public int quantity;
-	
+
 	public double totalValueInStock() {
 		return price * quantity;
 	}
-	
-	public void addProducts(int quatity) {
+
+	public void addProducts(int quantity) {
 		this.quantity += quantity;
 	}
-	
+
 	public void removeProducts(int quantity) {
 		this.quantity -= quantity;
 	}
-	
+
 	public String toString() {
-		return name
-			+ ", $ "
-			+ String.format("%.2f", price)
-			+ ", "
-			+ quantity
+		return name 
+			+ ", $ " 
+			+ String.format("%.2f", price) 
+			+ ", " 
+			+ quantity 
 			+ " units, Total: $ "
 			+ String.format("%.2f", totalValueInStock());
 	}
