d7fef61ac9465e8b3ac05f3f2c6f59e3fb152123
Caroline P. da Silva <carolinepedasil@gmail.com>
2022-05-26 23:19:07 -0300

    Java

---
+++ Udemy/Java COMPLETO Programação Orientada a Objetos/Construtores, palavra this, sobrecarga, encapsulamento/Exercicio01/entities/Product.java
@@ -0,0 +1,39 @@
+package entities;
+
+public class Product {
+
+	// Atributos:
+	public String name;
+	public double price;
+	public int quantity;
+	
+	// Construtor:
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	// Métodos:
+	public double totalValueInStock() {
+		return price * quantity;
+	}
+
+	public void addProducts(int quantity) {
+		this.quantity += quantity;
+	}
+
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity;
+	}
+
+	public String toString() {
+		return name 
+				+ ", $ " 
+				+ String.format("%.2f", price) 
+				+ ", " + quantity 
+				+ " units, Total: $ "
+				+ String.format("%.2f", totalValueInStock());
+	}
+
+}
