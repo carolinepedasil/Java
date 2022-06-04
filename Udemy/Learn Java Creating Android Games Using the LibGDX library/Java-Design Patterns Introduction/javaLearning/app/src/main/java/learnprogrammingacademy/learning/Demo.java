c4b918715b44e86e0ee5acf59eedf69a183bba46
Caroline P. da Silva <carolinepedasil@gmail.com>
2022-06-04 15:22:41 -0300

    Java

---
+++ Udemy/Learn Java Creating Android Games Using the LibGDX library/Java-Design Patterns Introduction/javaLearning/app/src/main/java/learnprogrammingacademy/learning/Demo.java
@@ -0,0 +1,23 @@
+package learnprogrammingacademy.learning;
+
+public class Demo {
+
+    static int lives = 3; // mutable (changeable)
+    static String playerName = "Caroline";
+    static boolean gameOver; // false by default
+
+    static final int LEVEL_COUNT = 4; // immutable (not changeable)
+
+    public static void main(String[] args) {
+        System.out.println("player " + playerName + " has " + lives + " lives left");
+
+        lives += 2; // lives = lives + 2;
+        playerName = playerName + " " + "Silva";
+        System.out.println("player " + playerName + " has " + lives + " lives left");
+        System.out.println("gameOver= " + gameOver);
+
+        System.out.println("player " + playerName + " needs to complete " + LEVEL_COUNT + " levels");
+        gameOver = true;
+        System.out.println("gameOver= " + gameOver);
+    }
+}
