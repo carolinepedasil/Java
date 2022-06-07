c2a6e3e59bca31d2876dba59ef019be7cf0c52cc
Caroline P. da Silva <carolinepedasil@gmail.com>
2022-06-06 23:36:16 -0300

    Java

--- Udemy/Learn Java Creating Android Games Using the LibGDX library/Java-Design Patterns Introduction/javaLearning/app/src/main/java/learnprogrammingacademy/learning/Demo.java
+++ Udemy/Learn Java Creating Android Games Using the LibGDX library/Java-Design Patterns Introduction/javaLearning/app/src/main/java/learnprogrammingacademy/learning/Demo.java
@@ -5,33 +5,37 @@ public class Demo {
     static int lives = 3; // mutable (changeable)
     static int score; // 0 by default
     static int coinCount = 90;
-
-    static int itemCount = 24;
-    static int itemsPerPage = 5;
+    static boolean gameOver; // false by dafault
 
     public static void main(String[] args) {
-        System.out.println("score= " + score);
+        score += 10;
+        coinCount = 110; // re-initializing
+        // lives = 0;
 
-        score += 10; // score = score + 10;
-        System.out.println("score= " + score + " lives= " + lives);
+        if(lives == 0) {
+            gameOver = true;
+        }
 
-        lives--; // lives = lives - 1; // lives -= 1;
-        System.out.println("score= " + score + " lives= " + lives);
+        if(coinCount > 100) {
+            System.out.println("gold medal");
+        } else if (coinCount > 80) {
+            System.out.println("silver medal");
+        } else if (coinCount > 70) {
+            System.out.println("bronze medal");
+        } else {
+            System.out.println("no medal");
+        }
 
-        score *= 2; // score = score * 2;
-        System.out.println("score= " + score + " lives= " + lives);
+        // if(gameOver == true)
+        if(gameOver) {
+            System.out.println("score= " + score);
+        } else {
+            System.out.println("lives= " + lives);
+        }
 
-        score /= 2; // score = score / 2;
-        System.out.println("score= " + score + " lives= " + lives);
-
-        coinCount += 40; // coinCount = coinCount + 40; // 90 + 40 = 130
-        System.out.println("coinCount= " + coinCount);
-
-        coinCount = coinCount % 100; // 130 / 100 = 1 -> 130 - 1 * 100 = 130 - 100 = 30
-        System.out.println("coinCount= " + coinCount);
-
-        //calculating remaining Items
-        int remainingItems = itemCount % itemsPerPage;
-        System.out.println("remainingItems= " + remainingItems);
+        //if(gameOver == false)
+        if(!gameOver) {
+            System.out.println("Game is not over");
+        }
     }
 }
