344f062b642aa9e6bb466417fe07e7396febb995
Caroline P. da Silva <carolinepedasil@gmail.com>
2022-06-06 23:06:32 -0300

    Java

--- Alura/Java e Orientação a Objetos/Java Polimorfismo/bytebank-herdado-conta/src/Conta.java
+++ Alura/Java e Orientação a Objetos/Java Polimorfismo/bytebank-herdado-conta/src/Conta.java
@@ -1,7 +1,7 @@
-public class Conta {
+public abstract class Conta {
 	
 	// Atributos
-	private double saldo;
+	protected double saldo;
 	private int agencia;
 	private int numero;
 	private Cliente titular;
@@ -18,9 +18,7 @@ public class Conta {
 	}
 	
 	// Métodos
-	public void deposita(double valor) {
-		this.saldo = this.saldo + valor;
-	}
+	public abstract void deposita(double valor);
 	
 	public boolean saca(double valor) {
 		if(this.saldo >= valor) {
