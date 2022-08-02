068d1d0c222e7381c8ce159fcc7ac8e540b9e730
Caroline P. da Silva <carolinepedasil@gmail.com>
2022-08-01 23:39:33 -0300

    Java

---
+++ Digital Innovation One/Projetos/Criando Seu Primeiro Projeto Prático com Orientação a Objetos em Java/loja/src/one/digitalinovation/laboojava/entidade/Pedido.java
@@ -0,0 +1,12 @@
+package one.digitalinovation.laboojava.entidade;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Classe que representa a entidade pedido, qual é a compra dos produtos por um cliente.
+public class Pedido {
+    //codigo
+    //cliente
+    //produtos
+    //total
+}
---
+++ Digital Innovation One/Projetos/Criando Seu Primeiro Projeto Prático com Orientação a Objetos em Java/loja/src/one/digitalinovation/laboojava/entidade/constantes/Genero.java
@@ -0,0 +1,25 @@
+package one.digitalinovation.laboojava.entidade.constantes;
+
+// Gêneros dos livros vendidos.
+public enum Genero {
+
+    DRAMA(15),
+
+    SUSPENSE(10),
+
+    ROMANCE(5);
+
+    private double fator;
+
+    /*
+		Construtor.
+		@param fator Valor por tipo que influencia no cálculo do frete.
+    */
+    Genero(double fator) {
+        this.fator = fator / 100;
+    }
+
+    public double getFator() {
+        return fator;
+    }
+}
