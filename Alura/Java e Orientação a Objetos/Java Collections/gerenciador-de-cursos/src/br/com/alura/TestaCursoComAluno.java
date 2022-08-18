76819f02036b842ea1eb4523a33e3897f37fb23c
Caroline P. da Silva <carolinepedasil@gmail.com>
2022-08-17 22:51:07 -0300

    Java

--- Alura/Java e Orientação a Objetos/Java Collections/gerenciador-de-cursos/src/br/com/alura/TestaCursoComAluno.java
+++ Alura/Java e Orientação a Objetos/Java Collections/gerenciador-de-cursos/src/br/com/alura/TestaCursoComAluno.java
@@ -23,6 +23,21 @@ public class TestaCursoComAluno {
 			System.out.println(a);
 		});
 		
+		System.out.println("O aluno " + a1 + " está matriculado?");
+		System.out.println(javaColecoes.estaMatriculado(a1));
+		
+		Aluno turini = new Aluno("Rodrigo Turini", 34672);
+		System.out.println("Este aluno está matriculado?");
+		System.out.println(javaColecoes.estaMatriculado(turini));
+		
+		System.out.println("O a1 é == ao Turini?");
+		System.out.println(a1 == turini);
+		System.out.println("O a1 é equals ao Turini?");
+		System.out.println(a1.equals(turini));
+		
+		// obrigatoriamente o seguinte é true:
+		System.out.println(a1.hashCode() == turini.hashCode());
+		
 	}
 
 }
