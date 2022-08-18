25089dd3e8c5b31145316a5519eb1cbc55f4e61d
Caroline P. da Silva <carolinepedasil@gmail.com>
2022-08-17 23:56:20 -0300

    Java

--- Alura/Java e Orientação a Objetos/Java Collections/gerenciador-de-cursos/src/br/com/alura/TestaCursoComAluno.java
+++ Alura/Java e Orientação a Objetos/Java Collections/gerenciador-de-cursos/src/br/com/alura/TestaCursoComAluno.java
@@ -1,5 +1,9 @@
 package br.com.alura;
 
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Vector;
+
 public class TestaCursoComAluno {
 	
 	public static void main(String[] args) {
@@ -19,6 +23,21 @@ public class TestaCursoComAluno {
 		javaColecoes.matricula(a3);
 		
 		System.out.println("Todos os alunos matriculados: ");
+		
+		Set<Aluno> alunos = javaColecoes.getAlunos();
+		Iterator<Aluno> iterador = alunos.iterator();
+		
+		while(iterador.hasNext()) {
+			Aluno proximo = iterador.next();
+			System.out.println(proximo);
+		}
+		
+		Vector<Aluno> vetor = new Vector<>();
+		
+//		for (Aluno a : javaColecoes.getAlunos()) {
+//			System.out.println(a);
+//		}
+		
 		javaColecoes.getAlunos().forEach(a -> {
 			System.out.println(a);
 		});
