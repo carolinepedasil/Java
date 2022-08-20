6e1bbae0c5d6aff76a637a6234acd204e1a77d01
Caroline P. da Silva <carolinepedasil@gmail.com>
2022-08-20 17:19:24 -0300

    Java

--- Alura/Java e Orientação a Objetos/Java 8/Java8/src/OrdernaStrings.java
+++ Alura/Java e Orientação a Objetos/Java 8/Java8/src/OrdernaStrings.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class OrdernaStrings {
 	
@@ -20,14 +21,32 @@ public class OrdernaStrings {
 			return 0;
 		});
 		
-		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+//		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+		
+		palavras.sort(Comparator.comparing(s -> s.length()));
+		palavras.sort(Comparator.comparing(String::length));
+		
+		Function<String, Integer> funcao = String::length;
+		Function<String, Integer> funcao2 = s -> s.length();
+		
+//		Function<String, Integer> funcao = new Function<String, Integer>() {
+//			@Override
+//			public Integer apply(String s) {
+//				return s.length();
+//			}
+//		};
+		
+		Comparator<String> comparador = Comparator.comparing(funcao);
+		palavras.sort(comparador);
 		
 		System.out.println(palavras);
 		
-		Consumer<String> impressor = s -> System.out.println(s);
+//		Consumer<String> impressor = s -> System.out.println(s);
+		Consumer<String> impressor = System.out::println;
 		palavras.forEach(impressor);
 		
-		palavras.forEach(s -> System.out.println(s));
+//		palavras.forEach(s -> System.out.println(s));
+		palavras.forEach(System.out::println);
 		
 	}
 
