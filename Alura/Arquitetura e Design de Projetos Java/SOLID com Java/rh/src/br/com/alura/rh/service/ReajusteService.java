8cb49058b5cae7607ffa6a36a72d004cfe0d596e
Caroline P. da Silva <carolinepedasil@gmail.com>
2022-08-23 22:20:36 -0300

    Java

--- Alura/Arquitetura e Design de Projetos Java/SOLID com Java/rh/src/br/com/alura/rh/service/ReajusteService.java
+++ Alura/Arquitetura e Design de Projetos Java/SOLID com Java/rh/src/br/com/alura/rh/service/ReajusteService.java
@@ -3,22 +3,26 @@ package br.com.alura.rh.service;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 import br.com.alura.rh.ValidacaoException;
 import br.com.alura.rh.model.Funcionario;
 
 public class ReajusteService {
+	
+	private List<ValidacaoReajuste> validacoes;
+	
+	public ReajusteService(List<ValidacaoReajuste> validacoes) {
+		this.validacoes = validacoes;
+	}
 
 	public void ReajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento) {
-		BigDecimal salarioAtual = funcionario.getSalario();
 		
-		BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
+		this.validacoes.forEach(v -> v.validar(funcionario, aumento));
 		
-		if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
-			throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
-		}
-		
-		BigDecimal salarioReajustado = salarioAtual.add(aumento);
+		BigDecimal salarioReajustado = funcionario.getSalario().add(aumento);
 		funcionario.atualizarSalario(salarioReajustado);
+		
 	}
 }
