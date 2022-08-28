4399152fe47699830c734c6bd557c62579448443
Caroline P. da Silva <carolinepedasil@gmail.com>
2022-08-28 20:48:04 -0300

    Java

--- Alura/Arquitetura e Design de Projetos Java/Design Patterns em Java II/loja/TestesImpostos.java
+++ Alura/Arquitetura e Design de Projetos Java/Design Patterns em Java II/loja/TestesImpostos.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import loja.imposto.CalculadoraDeImpostos;
 import loja.imposto.ICMS;
+import loja.imposto.ISS;
 import loja.orcamento.Orcamento;
 
 public class TestesImpostos {
@@ -11,7 +12,7 @@ public class TestesImpostos {
 	public static void main(String[] args) {
 		Orcamento orcamento = new Orcamento(new BigDecimal("100"), 1);
 		CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
-		System.out.println(calculadora.calcular(orcamento, new ICMS()));
+		System.out.println(calculadora.calcular(orcamento, new ICMS(new ISS(null))));
 	}
 
 }
