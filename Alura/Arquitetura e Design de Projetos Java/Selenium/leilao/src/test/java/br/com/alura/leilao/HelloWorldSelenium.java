305df4f1d70411f612096c8172e84e706692f823
Caroline P. da Silva <carolinepedasil@gmail.com>
2022-09-14 23:36:39 -0300

    Java

--- Alura/Arquitetura e Design de Projetos Java/Selenium/leilao/src/test/java/br/com/alura/leilao/HelloWorldSelenium.java
+++ Alura/Arquitetura e Design de Projetos Java/Selenium/leilao/src/test/java/br/com/alura/leilao/HelloWorldSelenium.java
@@ -4,14 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class HelloWordSelenium {
-	
+public class HelloWorldSelenium {
+
 	@Test
-	public void hello() {
+	public void helloTest() {
 		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
 		WebDriver browser = new ChromeDriver();
 		browser.navigate().to("http://localhost:8080/leiloes");
-		browser.quit();
 	}
 
 }
